6b5928c4ca0bef747a990a33c8f52487231f3c57
Talha <talhakucuk26@gmail.comgit config --global user.name Talhagit commitgit config --global user.email talhakucuk26@gmail.com>
2023-07-21 21:54:56 +0300

    sıralama yapan program

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("1. Sayı = ");
+        a = inp.nextInt();
+
+        System.out.print("2. Sayı = ");
+        b = inp.nextInt();
+
+        System.out.print("3. Sayı = ");
+        c = inp.nextInt();
+
+        if ((a > b ) && (a > c)) {
+            if (b > c) {
+                System.out.println("a > b > c");
+            } else {
+                System.out.println("a > c > b");
+            }
+        } else if ((b > a) && (b > c)) {
+              if (a > c){
+                  System.out.println("b > a > c");
+              }else {
+                  System.out.println("b > c > a");
+              }
+        } else {
+                if (a>b){
+                    System.out.println("c > b > a");
+                }else {
+                    System.out.println("c > a > b");
+                }
+
+        }
+    }
+}
